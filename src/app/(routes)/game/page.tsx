eefa9da19905ba177b8c8c52ad5afab79fe3b660
Abkenn <27154073+Abkenn@users.noreply.github.com>
2024-12-21 05:16:13 +0200

    Add game page

---
+++ src/app/(routes)/game/page.tsx
@@ -0,0 +1,10 @@
+
+export default function GamePage() {
+  return (
+    <div className="flex flex-col items-center min-h-screen px-8 py-24 space-y-8">
+      <h1 className="text-2xl font-bold text-center">Prime Rivals</h1>
+
+      <h2 className="text-xl font-semibold">Player 1 vs Player 2</h2>
+    </div>
+  );
+}

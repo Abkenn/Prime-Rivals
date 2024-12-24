0f74816ae8a9109e08d43a1e7afa66f93b486f4a
Abkenn <27154073+Abkenn@users.noreply.github.com>
2024-12-24 05:18:44 +0200

    Update game pages routing

---
+++ src/app/(routes)/game/[roomCode]/page.tsx
@@ -0,0 +1,14 @@
+import { getRoom } from "@/app/_actions/roomActions";
+
+export default async function GameRoomPage({ params }: { params: Promise<{ roomCode: string }> }) {
+  const roomCode = (await params).roomCode;
+  const roomData = await getRoom(roomCode ?? '');
+
+  return (
+    <div className="flex flex-col items-center min-h-screen px-8 py-24 space-y-8">
+      <h1 className="text-2xl font-bold text-center">Prime Rivals</h1>
+
+      <h2 className="text-xl font-semibold">{roomData.hostPlayerName} vs {roomData.secondPlayerName}</h2>
+    </div>
+  );
+}
--- src/app/(routes)/game/page.tsx
+++ src/app/(routes)/game/page.tsx
@@ -1,14 +1,10 @@
-import { getRoom } from "@/app/_actions/roomActions";
 
-export default async function GamePage(props: { searchParams: Promise<Record<string, string | undefined>> }) {
-  const searchParams = await props.searchParams;
-  const roomData = await getRoom(searchParams.roomCode ?? '');
-
+export default function GamePage() {
   return (
     <div className="flex flex-col items-center min-h-screen px-8 py-24 space-y-8">
       <h1 className="text-2xl font-bold text-center">Prime Rivals</h1>
 
-      <h2 className="text-xl font-semibold">{roomData.hostPlayerName} vs {roomData.secondPlayerName}</h2>
+      <h2 className="text-xl font-semibold">Room Not Found</h2>
     </div>
   );
 }
